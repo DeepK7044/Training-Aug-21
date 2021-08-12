a08a5ac3fef43d904d557d9780bdfc9e24cdc323
Deep Parmar <deep.parmar@rxteam376.onmicrosoft.com>
2021-08-12 19:28:31 +0530

    REFS: #482 and fixes: #482 Completed Assignment

--- SQL/Deep Parmar/Day 3/Assignment Query_1.sql
+++ SQL/Deep Parmar/Day 3/Assignment Query_1.sql
@@ -1,7 +1,6 @@
 -- Assignment:
 
--- [1] Write a query that displays the FirstName and the length of the FirstName for all employees whose name starts with the letters ‘A’, ‘J’ or ‘M’.
---     Give each column an appropriate label. Sort the results by the employees’ FirstName.
-
+-- [1] Write a query that displays the FirstName and the length of the FirstName for all employees whose name starts with the letters ï¿½Aï¿½, ï¿½Jï¿½ or ï¿½Mï¿½.
+--     Give each column an appropriate label. Sort the results by the employeesï¿½ FirstName.
 
 SELECT [Name]=FirstName,LEN(FirstName) AS Length_Of_FirstName FROM Employees WHERE FirstName LIKE 'A%' OR FirstName LIKE 'J%'OR FirstName LIKE 'M%'  ORDER BY FirstName;
