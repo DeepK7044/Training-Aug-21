64c2367d9524973347f7a8f09d8bb6e026a1c20d
Deep Parmar <deep.parmar@rxteam376.onmicrosoft.com>
2021-08-18 16:46:39 +0530

    REFS: #547 and fixes #547 Practiced all joins

--- SQL/Deep Parmar/Day 5/Assignment Query_3.sql
+++
@@ -1,6 +0,0 @@
---Assignment:
-
---[3] Select first_name, incentive amount from employee and incentives table for all employees even if they didn’t get incentives.
-
-		SELECT EMP.FIRST_NAME,INS.INCENTIVE_AMOUNT FROM Employee AS EMP LEFT OUTER JOIN Incentives AS INS 
-		ON EMP.EMPLOYEE_ID=INS.EMPLOYEE_REF_ID
--- SQL/Deep Parmar/Day 5/Assignment Query_5.sql
+++
@@ -1,7 +0,0 @@
---Assignment:
-
---[5] Select first_name, incentive amount from employee and incentives table for all employees even if they didn’t get incentives 
---    and set incentive amount as 0 for those employees who didn’t get incentives.
-
-	  SELECT EMP.FIRST_NAME,ISNULL(INS.INCENTIVE_AMOUNT,0) FROM Employee AS EMP LEFT OUTER JOIN Incentives AS INS 
-	  ON EMP.EMPLOYEE_ID=INS.EMPLOYEE_REF_ID
